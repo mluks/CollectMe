0754fc6382bee49d1494be7ff2910d3961c8ef44
Mary Luksetich <meluksetich.dev@gmail.com>
2017-09-11 10:50:06 -0500

    First commit

---
+++ DB/Zone.lua
@@ -0,0 +1,35 @@
+local CollectMe = LibStub("AceAddon-3.0"):GetAddon("CollectMe")
+local Data = CollectMe:GetModule("Data")
+
+CollectMe.ZoneDB = CollectMe:NewModule("ZoneDB")
+
+function CollectMe.ZoneDB:OnInitialize()
+    self.list, self.order = {}, {}
+    self.loaded = false
+end
+
+function CollectMe.ZoneDB:GetList()
+    if self.loaded == false then
+        for _,v in ipairs(Data.Zones) do
+            self.list[v] = GetMapNameByID(v)
+            table.insert(self.order, v)
+        end
+        self.loaded = true
+    end
+    return self.list, self.order
+end
+
+function CollectMe.ZoneDB:Current()
+    SetMapToCurrentZone()
+    local zone = GetCurrentMapAreaID()
+    return zone
+end
+
+function CollectMe.ZoneDB:PrintZones()
+    for i = 1, 1200, 1 do
+        SetMapByID(i)
+        if GetMapNameByID(i) ~= nil then
+            CollectMe:Print(i.. " - " ..GetMapNameByID(i))
+        end
+    end
+end
